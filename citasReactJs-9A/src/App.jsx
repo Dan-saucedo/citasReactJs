1c501dcda7c874629b3bb9af6c2f7bf91e47bd98
Daniel Saucedo Avila <st4587@utr.edu.mx>
2026-09-22 13:49:23 -0600

    Initial configuration of the interface

--- citasReactJs-9A/src/App.jsx
+++ citasReactJs-9A/src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import './css/app.css';
 
 function App() {
+
+  const [visible, setModalVisible] = useState(false)
   return (
-    <h1> Hola mundo : citas JS</h1>
+    <main className = "container">
+      <h1 className = "titulo">
+        Administrador de citas <span className="titulo-bold"> Veterinario </span>
+      </h1>
+      <button type='button' className='btn-nueva-cita' onClick={() => setModalVisible(true)}> 
+        <span className="btn-texto-nueva-cita"> Nueva cita </span>
+      </button>
+    </main>
   )
 }
 
